d1fa18c0154544894a47ad6ae8ec54965d485eb4
marat.zinatulin <marat.zinatulin@fido.uz>
2023-06-08 15:54:13 +0500

    next commit

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -13,12 +13,31 @@ export async function POST(
 ) {
   const {email, name, password}: IRegisterForm =  await req.json();
   const prisma = new PrismaClient();
-  const user = await prisma.user.create({
-    data: {
-      email: email,
-      name: name,
-      password: await hash(password)
+  let user;
+  try {
+    const oldEmail = await prisma.user.findUnique({
+      where: {email: email}
+    });
+    if (oldEmail) {
+      return NextResponse.json({message: 'Email is already busy.'}, {status: 422});
     }
-  });
+    const oldName = await prisma.user.findUnique({
+      where: {name: name}
+    });
+    if (oldName) {
+      return NextResponse.json({message: 'User already exists.'}, {status: 422});
+    }
+    user = await prisma.user.create({
+      data: {
+        email: email,
+        name: name,
+        password: await hash(password)
+      }
+    });
+  } catch (e) {
+    return NextResponse.json({message: 'Problems with DB.'}, {status: 500});
+  }
+
+
   return NextResponse.json(user, {status: 200});
 }
